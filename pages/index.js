e8a220b21e598fa4e0638cd7d1f30334016d097a
Andrew <yudishino0826@gmail.com>
2023-03-19 12:59:36 +0900

    index.js/axios/api url 変化

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Index({ data }) {
 
 export async function getServerSideProps() {
   try {
-    const response = await axios.get("http://localhost:3000/api/test");
+    const response = await axios.get("/api/test");
     const data = response.data;
 
     return {
